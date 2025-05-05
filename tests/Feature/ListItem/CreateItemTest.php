86d61b650ef8f53366df3ae7d10c418d81d3510d
Mike Thomas <mikethomas84@gmail.com>
2025-05-05 16:45:58 +0100

    ListItem is shown on the page test

--- tests/Feature/ListItem/CreateItemTest.php
+++ tests/Feature/ListItem/CreateItemTest.php
@@ -14,7 +14,7 @@ test('ListItem can be created', function () {
         'user_id' => $user->id,
     ]);
 
-    $component = Volt::test('shoppinglist',[ 'list' => $shoppingList ])
+    $component = Volt::test('shoppinglist', [ 'list' => $shoppingList ])
         ->set('item_title', 'My List Item')
         ->set('item_price', '10.00')
         ->call('addListItem');
@@ -28,4 +28,25 @@ test('ListItem can be created', function () {
         'price' => '10.00',
         'shopping_list_id' => $shoppingList->id,
     ]);
+});
+
+test('ListItem is listed in the shopping list', function () {
+    $this->actingAs($user = User::factory()->create());
+
+    // Create a new shopping list first
+    $shoppingList = ShoppingList::factory()->create([
+        'user_id' => $user->id,
+    ]);
+
+    // Create a new list item
+    $listItem = $shoppingList->items()->create([
+        'title' => 'My List Item',
+        'price' => '10',
+    ]);
+
+    $component = Volt::test('shoppinglist', [ 'list' => $shoppingList ]);
+
+    $component
+        ->assertSee($listItem->title)
+        ->assertSee($listItem->price);
 });
